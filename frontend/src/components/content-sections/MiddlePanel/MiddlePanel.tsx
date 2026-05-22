3dfd356684ffc3e4934b563798c3c54d927c2e36
Zoe <zmb6893@g.rit.edu>
2026-05-22 15:05:25 -0700

    Added in cartodb dark theme map with react leaflet

--- frontend/src/components/content-sections/MiddlePanel/MiddlePanel.tsx
+++ frontend/src/components/content-sections/MiddlePanel/MiddlePanel.tsx
@@ -1,25 +1,42 @@
-import './MiddlePanel.css'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import './MiddlePanel.css';
+import 'leaflet/dist/leaflet.css';
+import { LatLngTuple } from 'leaflet';
 
 function MiddlePanel () {
+	// Pullman coordinates for now
+	const position: LatLngTuple = [46.73333, -117.16861];
+
 	return (
 		<div className="middlePanel">
-			<p>
-				INCOMPLETE
-			</p>
-			<p>
-				This section will contain the following:
-			</p>
-			<ul>
-				<li>
-					A search bar that will highlight requested assets/devices/locations
-				</li>
-				<li>
-					A filter panel that allows you to filter by asset type, warning status, and version
-				</li>
-				<li>
-					A node graph of the assets in the grid network.
-				</li>
-			</ul>
+			<div className="top-layer">
+				<p>
+					INCOMPLETE
+				</p>
+				<p>
+					This section will contain the following:
+				</p>
+				<ul>
+					<li>
+						A search bar that will highlight requested assets/devices/locations
+					</li>
+					<li>
+						A filter panel that allows you to filter by asset type, warning status, and version
+					</li>
+					<li>
+						A node graph of the assets in the grid network.
+					</li>
+				</ul>
+			</div>
+			<div className="bottom-layer">
+				<MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+					<TileLayer
+						attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+						url='https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
+					/>
+				</MapContainer>
+			</div>
+			
 		</div>
 	)
 }
